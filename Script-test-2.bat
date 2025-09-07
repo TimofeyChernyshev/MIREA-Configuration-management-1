09056a7a41a0682c95031087f73ea577692346f6
unknown <timkachern@mail.ru>
2025-09-07 10:04:44 +0300

    Stage 3. feat: Add compiled file

--- Script-test-2.bat
+++
@@ -1,8 +0,0 @@
-echo "Test 1: Calling script without arguments"
-go run main.go -script
-
-echo "Test 2: Script with wrong command"
-go run main.go -script Script-startup-wrong.txt
-
-echo "Test 3: Script with correct commands"
-go run main.go -script Script-startup-right.txt
